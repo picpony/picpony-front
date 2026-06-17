60c4010b4d127f18282b2ef05ab9b95ed9bae1c1
Csituka_D <694428809@qq.com>
2026-06-17 19:34:12 +0800

    微调

--- app/layout.tsx
+++ app/layout.tsx
@@ -55,7 +55,7 @@ export default async function RootLayout({
           height={3}
           crawl={true}
           showSpinner={false}
-          easing="ease"
+          easing="ease-in-out"
           speed={200}
         />
         <AppLayout initialCollapsed={sidebarCollapsed} initialDark={darkMode}>
