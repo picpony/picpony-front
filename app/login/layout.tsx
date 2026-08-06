17104367c52157fef1a5bb544ccfffc6c2d05be1
Csituka_D <694428809@qq.com>
2026-08-06 14:05:10 +0800

    clean

--- app/login/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { Metadata } from 'next';
-
-export const metadata: Metadata = {
-  title: '登录',
-};
-
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return <>{children}</>;
-}
--- app/reset-password/layout.tsx
+++
@@ -1,9 +0,0 @@
-import type { Metadata } from 'next';
-
-export const metadata: Metadata = {
-  title: '找回密码',
-};
-
-export default function ResetPasswordLayout({ children }: { children: React.ReactNode }) {
-  return children;
-}
